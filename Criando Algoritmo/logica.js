8a70b052f6cf5b4f81e19833abca46bf1354b49d
Douglas <douglasmiguel@msn.com>
2023-09-11 04:26:08 -0300

    Algoritmo add

---
+++ Criando Algoritmo/logica.js
@@ -0,0 +1,21 @@
+
+Var
+EC: Inteiro
+EC = 1
+Inicio 
+Escreva "Digite o estado civil (1 - solteiro, 2 - casado, 3 - outros)"
+Leia EC 
+Se EC = 1 ENTAO 
+  escreva "Solteiro"
+  SENAO 
+    SE EC = 2 ENTAO
+    Escreva "Casado"
+    SENAO
+    SE EC = 3 ENTAO
+    Escreva "Outros"
+    SENAO
+    Escreva "VAlor Invalido"
+    FIM SE
+  FIM SE
+FIM  
+
